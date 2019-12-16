e3152e2805c54eb54ce0beaaa5f29b0877c76313
Jeremiah John <jeremiahjohn08706@gmail.com>
2019-12-16 12:22:43 -0600

    finished try-catch block in evaluation.cpp

--- evaluation.cpp
+++ evaluation.cpp
@@ -12,12 +12,16 @@ int evalLoop(stringstream os, int16_t *stack, int *stackCount)
 	if(currentTerm=="+")
 	    addition(stack,stackCount);
 	else if(currentTer=="-")
-            addition(stack,stackCount);
+            subtraction(stack,stackCount);
 	else
 	{
             try
 	    {
-                
+                stack[stackCount] = std::stoi(currentTerm); 
+	    }
+	    catch(std::invalid_argument)
+	    {
+                std::cout << "There was an invalid argument in your input!" << std::endl;
 	    }
 	}	
     }
@@ -26,7 +30,7 @@ void evaluateStatement(std::string statement)
 {
     int16_t stack[100];
     int result = 0;
-    int stackCount; //this tells us how many items are in the stack, which we will need to know for when we need to pull two numbers out of the stack for operation
+    int stackCount = 0; //this tells us how many items are in the stack, which we will need to know for when we need to pull two numbers out of the stack for operation
     stringstream os;
     os << statement << std::endl;
     result = evalLoop(os, &stack, &stackCount)
